54e7deb833b6166d35cda22c1a32b0fb650a8abf
wechan <a421083@qq.com>
2017-08-29 10:43:51 +0800

    注解拦截

---
+++ src/Events/InterceptCenter.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+/*
+ * This file is part of eelly package.
+ *
+ * (c) eelly.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Eelly\DevTools\Events;
+
+/**
+ * 拦截器管理中心
+ * @version 1.0
+ */
+class InterceptCenter
+{
+    /**
+     * 事件管理
+     * @var object 
+     */
+    public $eventsManager;
+    
+    public function __construct($event) 
+    {
+        $this->eventsManager = $event;
+    }
+    
+    /**
+     * 注册db事件监听事件
+     * 
+     * @throws \Exception
+     */
+    public function registDbListener()
+    {
+        $this->eventsManager->attach('db', new DbListerner());
+    }
+    
+    /**
+     * 注册注解监听事件
+     * 
+     * @throws \Exception
+     */
+    public function registAnnotation()
+    {
+        $annotation = new Annotation();
+        $annotation->verify();
+    }
+}
+
